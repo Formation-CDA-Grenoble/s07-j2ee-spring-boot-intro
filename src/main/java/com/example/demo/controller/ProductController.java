6719b7dff26df736854aea81494f34d06bd99c5a
Romain Tello <romain.tello@gmail.com>
2020-01-29 16:12:38 +0100

    Add get product by id

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -6,7 +6,10 @@ import com.example.demo.model.Product;
 import com.example.demo.repository.ProductRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/products")
@@ -19,5 +22,13 @@ public class ProductController {
     @GetMapping("/")
     public List<Product> getAllProducts() {
         return productRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable(value = "id") Long productId) {
+        Product product = productRepository.findById(productId).orElseThrow(
+            () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found")
+        );
+        return product;
     }
 }
